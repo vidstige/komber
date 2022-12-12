4f89195c5775c76a8db7e7773fd9d406861bf36d
Samuel Carlsson <samuel.carlsson@volumental.com>
2022-12-12 21:06:19 +0100

    inputs/outputs

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,22 @@
 import ko from "knockout";
 import { Draggable, Layout, Mouse } from "./drag";
 
+interface Input {
+
+}
+interface Output {
+
+}
+
 class Card implements Draggable {
     title: ko.Observable<string>;
+    inputs: Input[];
+    outputs: Output[];
     layout: Layout;
-    constructor(title: string, layout: Layout) {
+    constructor(title: string, layout: Layout, inputs: Input[], outputs: Output[]) {
         this.title = ko.observable(title);
+        this.inputs = inputs;
+        this.outputs = outputs;
         this.layout = layout;
     }
 }
@@ -17,12 +28,12 @@ class App {
         this.cards = ko.observableArray();
         this.mouse = new Mouse();
     }
-    card(title: string): Card {
-        return new Card(title, new Layout(80, 40));
+    card(title: string, inputs: Input[], outputs: Output[]): Card {
+        return new Card(title, new Layout(80, 40), inputs, outputs);
     }
 }
 
 const app = new App();
-app.cards.push(app.card('virga'));
+app.cards.push(app.card('virga', [1, 2], ['a']));
 ko.applyBindings(app);
 
